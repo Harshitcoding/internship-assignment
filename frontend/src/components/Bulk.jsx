b4ddc13dc0fe2d73b12351e1e1a8778c0976237e
Harshitcoding <khandelwalharshit96@gmail.com>
2024-06-20 09:50:13 +0530

    changed Bulk

--- frontend/src/components/Bulk.jsx
+++ frontend/src/components/Bulk.jsx
@@ -24,23 +24,31 @@ const ProductList = () => {
 
   return (
     <div className="bg-gray-200 p-4 rounded shadow-md mx-auto max-w-2xl">
-      <h2>All Products</h2>
-      <ul className="list-none p-0">
-        {products.map(product => (
-          <li key={product.productId} className="flex justify-between items-center py-2 border-b border-gray-300 hover:bg-gray-100">
-            <div className="flex items-center">
-              <span className="text-sm mr-2 font-medium">Company:</span>
-              <span>{product.company ? product.company : 'N/A'}</span>  {/* Handle missing company data */}
-            </div>
-            <div className="flex items-center">
-              <span className="text-sm mr-2 font-medium">Price:</span>
-              <span>{product.price}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    <h2>All Products</h2>
+    <ul className="list-none p-0">
+      {products.map(product => (
+        <li key={product.productId} className="flex justify-between items-center py-2 border-b border-gray-300 hover:bg-gray-100">
+          <div className="flex items-center">
+            <span className="text-sm mr-2 font-medium">Company:</span>
+            <span>{product.company ? product.company : 'N/A'}</span>  {/* Handle missing company data */}
+          </div>
+          <div className="flex items-center">
+            <span className="text-sm mr-2 font-medium">Price:</span>
+            <span>{product.price}</span>
+          </div>
+          <div className="flex items-center">
+            <span className="text-sm mr-2 font-medium">Rating:</span>
+            <span>{product.rating.$numberDecimal}</span>
+          </div>
+          <div>
+            <span className="text-sm mr-2 font-medium">Featured:</span>
+            <span>{product.featured ? 'Yes' : 'No'}</span>
+          </div>
+        </li>
+      ))}
+    </ul>
+  </div>
+    );
 };
 
 export default ProductList;
